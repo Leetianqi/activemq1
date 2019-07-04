c2a1da690dd9ce73ad2057f8ac87e97a89ba645c
litianqi <litianqi@hikvision.com>
2019-07-04 17:07:01 +0800

    activemq

--- src/main/java/cn/how2j/utils/ActiveMqUtil.java
+++ src/main/java/cn/how2j/utils/ActiveMqUtil.java
@@ -6,8 +6,9 @@ import javax.swing.*;
 
 public class ActiveMqUtil {
     public  static void checkServer(){
+        //要想在127.0.0.1:8161上面看到这个过程,必须开启activemq服务(D:\software\middlewareForMessage\apache-activemq-5.15.8-bin\apache-activemq-5.15.8\bin\win64)
         if (NetUtil.isUsableLocalPort(8161)){
-            //弹出一个名为“Message”的信息消息对话框
+            //弹出一个名为“Message”的信息消息对话框(可以在页面上看到的),其实用system也是可以的
             JOptionPane.showMessageDialog(null, "ActiveMQ 服务器未启动 ");
             System.exit(1);
         }
